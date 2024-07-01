a94938661d9354a04a4a5c4005273cf229db0104
oliceglad <vycheslavofficial@gmail.com>
2024-07-01 14:42:30 +0400

    added downloadButton UI

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,9 +1,11 @@
 import { ButtonCard } from "../components/ButtonCard/ButtonCard";
 import { InfoCard } from "../components/InfoCard/InfoCard";
+import { DownloadButton } from "../components/UI/downloadButton/downloadButton";
 
 export const MainPage = () => {
   return (
     <div className="main">
+      <DownloadButton />
     </div>
   );
 };
