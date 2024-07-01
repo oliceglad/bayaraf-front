6d421e8f3c1efaa499bfe0504ccd97437fe3bef4
oliceglad <vycheslavofficial@gmail.com>
2024-07-01 15:33:30 +0400

    added new style for mainpage

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -3,9 +3,9 @@ import s from './Title.module.scss'
 export const Title = ({children}) => {
     return (
         <div className={s.title}>
-            <a href="#" className={s.title__link}>
+            <h2 href="#" className={s.title__link}>
                 {children}
-            </a>
+            </h2>
         </div>
     )
 }
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -5,11 +5,55 @@ import { ListOfCalc } from "../components/ListOfCalc/ListOfCalc";
 import { InfoOfCalc } from "../components/InfoOfCalc/InfoOfCalc";
 import { calcList, ordererList } from "../data/data";
 
-
 export const MainPage = () => {
   return (
-    <div className="main">
-      <InfoOfCalc infoList={calcList} />
-    </div>
+    <main className="main">
+      <section className="main__top">
+        <div className="main__top__left">
+          <Title>Список тендерных расчетов</Title>
+          <ListOfCalc />
+        </div>
+        <div className="main__top__right">
+          <Title>Основная информация для расчетной таблицы</Title>
+          <div className="main__top__right__flex">
+            <InfoOfCalc infoList={calcList} />
+            <InfoOfCalc infoList={ordererList} />
+          </div>
+        </div>
+      </section>
+
+      <section className="main__middle">
+        <Title>Расчетная таблица тендерной сессии</Title>
+      </section>
+
+      <section className="main__bottom">
+        <ul className="main__bottom__list">
+          <li className="main__bottom__list__item">
+            <InfoCard head="Начальная максимальная цена" info="300 000 ₽" />
+          </li>
+          <li className="main__bottom__list__item">
+            <InfoCard head="Начальная максимальная цена" info="300 000 ₽" />
+          </li>
+          <li className="main__bottom__list__item">
+            <InfoCard head="Начальная максимальная цена" info="300 000 ₽" />
+          </li>
+          <li className="main__bottom__list__item">
+            <InfoCard head="Начальная максимальная цена" info="300 000 ₽" />
+          </li>
+          <li className="main__bottom__list__item">
+            <InfoCard head="Начальная максимальная цена" info="300 000 ₽" />
+          </li>
+          <li className="main__bottom__list__item">
+            <InfoCard head="Начальная максимальная цена" info="300 000 ₽" />
+          </li>
+          <li className="main__bottom__list__item">
+            <InfoCard head="Начальная максимальная цена" info="300 000 ₽" />
+          </li>
+          <li className="main__bottom__list__item">
+            <DownloadButton />
+          </li>
+        </ul>
+      </section>
+    </main>
   );
 };
