af33e7164ef41c050a5fb6784ff60b126732f641
oliceglad <vycheslavofficial@gmail.com>
2024-07-03 15:15:48 +0400

    added item card

--- src/pages/ItemsPage.jsx
+++ src/pages/ItemsPage.jsx
@@ -1,8 +1,13 @@
+import { ItemCard } from "../components/ItemCard/ItemCard";
+
 export const ItemsPage = () => {
   return (
     <main className="items">
       <section>
-        
+        <ItemCard
+          title="Видеокарта KFA2 GeForce 210 [21GGF4HI00NK] [PCI-E 2.0 1 ГБ GDDR3, 64 бит, DVI-I, HDMI, VGA (D-Sub), GPU 520 МГц]"
+          price="2 980"
+        />
       </section>
     </main>
   );
