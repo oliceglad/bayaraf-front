a36b6b5c9838cfc6a078478dc4532ec4d7398fd7
oliceglad <vycheslavofficial@gmail.com>
2024-07-26 16:35:39 +0400

    refactor editableinfo"

--- src/components/UI/editableInfo/EditableInfo.jsx
+++ src/components/UI/editableInfo/EditableInfo.jsx
@@ -1,93 +1,65 @@
-import { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import s from "./editableInfo.module.scss";
 
 export const EditableInfo = ({ info, setInfo, theme, isSmall = false }) => {
-  const [isEditing, setIsEditing] = useState(false);
+  const inputRef = useRef(null);
 
-  const handleEditClick = () => {
-    setIsEditing(true);
+  const handleClick = () => {
+    setTimeout(() => {
+      inputRef.current && inputRef.current.focus();
+    }, 0);
   };
 
   const handleInputChange = (e) => {
     setInfo(e.target.value);
   };
 
-  const handleSaveClick = () => {
-    setInfo(info);
-    setIsEditing(false);
-  };
-
   const handleKeyDown = (e) => {
     if (e.key === "Enter") {
-      handleSaveClick();
+      const inputs = document.querySelectorAll("input");
+      const currentInputIndex = Array.from(inputs).indexOf(inputRef.current);
+      if (currentInputIndex !== -1 && currentInputIndex < inputs.length - 1) {
+        const nextInput = inputs[currentInputIndex + 1];
+        nextInput.focus();
+      }
     }
   };
 
   return (
-    <div>
-      {isEditing ? (
-        <>
-          <input
-            type="text"
-            value={info}
-            onChange={handleInputChange}
-            className={`${s.editableInfo__input} ${theme}`}
-            onKeyDown={handleKeyDown}
-            style={
-              isSmall
-                ? { width: "60%", fontSize: "14px", borderColor: "black" }
-                : theme === "light"
-                ? { borderColor: "black" }
-                : {}
-            }
-          />
-          <span onClick={handleSaveClick} className={s.editableInfo__button}>
-            <svg
-              weight={isSmall ? "10px" : "17px"}
-              height={isSmall ? "10px" : "17px"}
-              viewBox="0 0 17 17"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M15.9819 4.70291L16.237 4.44776C17.2543 3.43047 17.2543 1.78026 16.237 0.76297C15.2197 -0.254323 13.5695 -0.254323 12.5522 0.76297L12.2971 1.01812L15.9819 4.70291Z"
-                fill="#0075FF"
-              />
-              <path
-                d="M11.1604 2.15471L0.825101 12.49L0 16.9999L4.50989 16.1748L14.8452 5.83949L11.1604 2.15471Z"
-                fill="#0075FF"
-              />
-            </svg>
-          </span>
-        </>
-      ) : (
-        <>
-          <div
-            className={` ${s.editableInfo__info} ${theme}`}
-            style={isSmall ? { fontSize: "14px" } : {}}
+    <div className={s.editableInfo}>
+      <>
+        <input
+          ref={inputRef}
+          type="text"
+          value={info}
+          onChange={handleInputChange}
+          className={`${s.editableInfo__input} ${theme} ${theme === "light" ? s.light : ""}`}
+          onKeyDown={handleKeyDown}
+          style={
+            isSmall
+              ? {display: "inline-block", width: "50px", fontSize: "14px"}
+              : {}
+          }
+        />
+        <span onClick={handleClick} className={s.editableInfo__button}>
+          <svg
+            width={isSmall ? "10px" : "17px"}
+            height={isSmall ? "10px" : "17px"}
+            viewBox="0 0 17 17"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            {info}
-          </div>
-          <span onClick={handleEditClick} className={s.editableInfo__button}>
-            <svg
-              width={isSmall ? "10px" : "17px"}
-              height={isSmall ? "10px" : "17px"}
-              viewBox="0 0 17 17"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M15.9819 4.70291L16.237 4.44776C17.2543 3.43047 17.2543 1.78026 16.237 0.76297C15.2197 -0.254323 13.5695 -0.254323 12.5522 0.76297L12.2971 1.01812L15.9819 4.70291Z"
-                fill="#0075FF"
-              />
-              <path
-                d="M11.1604 2.15471L0.825101 12.49L0 16.9999L4.50989 16.1748L14.8452 5.83949L11.1604 2.15471Z"
-                fill="#0075FF"
-              />
-            </svg>
-          </span>
-        </>
-      )}
+            <path
+              d="M15.9819 4.70291L16.237 4.44776C17.2543 3.43047 17.2543 1.78026 16.237 0.76297C15.2197 -0.254323 13.5695 -0.254323 12.5522 0.76297L12.2971 1.01812L15.9819 4.70291Z"
+              fill="#0075FF"
+            />
+            <path
+              d="M11.1604 2.15471L0.825101 12.49L0 16.9999L4.50989 16.1748L14.8452 5.83949L11.1604 2.15471Z"
+              fill="#0075FF"
+            />
+          </svg>
+        </span>
+      </>
     </div>
   );
 };
